15e4ed241e4cc6c54a5eb52345586e3644fa692c
Belal Islam <belalislam294@gmail.com>
2025-05-12 00:35:12 +0600

    fdsf

--- users/email.py
+++ users/email.py
@@ -1,10 +1,9 @@
-# users/email.py
 from djoser.email import ActivationEmail
 from django.conf import settings
 
 class CustomActivationEmail(ActivationEmail):
     def get_context_data(self):
         context = super().get_context_data()
-        frontend_url = settings.FRONTEND_URL
-        context["url"] = f"{frontend_url}/#/activate/{context['uid']}/{context['token']}"
+        frontend_url = settings.FRONTEND_URL 
+        context["url"] = f"{frontend_url}{context['link']}" 
         return context
