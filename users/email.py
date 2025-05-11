b36d507dbefc639f4b9b170bd8759e601beadda9
Belal Islam <belalislam294@gmail.com>
2025-05-11 23:59:31 +0600

    add custom email

--- users/email.py
+++ users/email.py
@@ -1,3 +1,4 @@
+# users/email.py
 from djoser.email import ActivationEmail
 from django.conf import settings
 
@@ -5,5 +6,5 @@ class CustomActivationEmail(ActivationEmail):
     def get_context_data(self):
         context = super().get_context_data()
         frontend_url = settings.FRONTEND_URL
-        context["url"] = f"{frontend_url}/activate/{context['uid']}/{context['token']}"
+        context["url"] = f"{frontend_url}/#/activate/{context['uid']}/{context['token']}"
         return context
