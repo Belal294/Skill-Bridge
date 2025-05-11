22fe1137cfc8dc778ec21369110b34aec3a4e9a9
Belal Islam <belalislam294@gmail.com>
2025-05-11 23:41:26 +0600

    email bugs fix

---
+++ users/email.py
@@ -0,0 +1,9 @@
+from djoser.email import ActivationEmail
+from django.conf import settings
+
+class CustomActivationEmail(ActivationEmail):
+    def get_context_data(self):
+        context = super().get_context_data()
+        frontend_url = settings.FRONTEND_URL
+        context["url"] = f"{frontend_url}/activate/{context['uid']}/{context['token']}"
+        return context
