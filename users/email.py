8ed72b54fef02431fd6bc86b853ba8742f4d2029
Belal Islam <belalislam294@gmail.com>
2025-05-13 16:46:15 +0600

    bug fixx

--- users/email.py
+++ users/email.py
@@ -1,19 +1,24 @@
-# users/email.py
+
 from djoser.email import ActivationEmail
 from django.conf import settings
 
 class CustomActivationEmail(ActivationEmail):
     def get_context_data(self):
+        
         context = super().get_context_data()
-        frontend_url = settings.FRONTEND_URL  
+        print("check:", context["url"])
+        frontend_url = settings.FRONTEND_URL 
+        print("check2343", frontend_url) 
         
         if context['url'].startswith('/'):
             context['url'] = context['url'][1:] 
         
-    
-        context["url"] = f"{frontend_url}/{context['url']}"
+        a = frontend_url
+        b = context["url"]
+        context["url"] = f"{a}/{b}"
         # context["url"] = f"{context['url']}"
         
-        print(" Activation link being sent:", context["url"])  
+        
+        print("Activation link being sent:", context["url"])  
         
         return context
